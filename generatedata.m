efd7240fa8c7d83e64ff97407a12dcad7c68f8fc
Clémence Réda <clmence.reda@ens-cachan.fr>
2016-12-11 18:53:48 +0100

    Debug : changed dimensions of x y

--- generatedata.m
+++ generatedata.m
@@ -1,6 +1,8 @@
 function [x y] = generatedata(n, d, m1=0.5, m2=1, class1=ceil(n/2))
 % GENERATEDATA Generates two classes of samples and labels in
-% number n and of dimension d with two Gaussian functions of different moments.
+% number n and of dimension d with two Gaussian functions of
+% different moments.
+% x of dimension d x n and y of dimension 1 x n
 % [x, y] = generatedata(n, d) Generates two classes (first class
 % having ceil(n/2) elements) with moments 0.5 and 1
 % [x, y] = generatedata(n, d, m1) Generates two classes (first class
@@ -24,7 +26,9 @@ for i=1:d
         m2 += 1;
 end
 
-% n x d matrix 
-x = [x1; x2];
-% y(i) corresponds to label of point x(i, :)
-y = [ones(class1, 1); (-ones(class2, 1))];
+% d x n matrix 
+x = [x1; x2]';
+
+% 1 x d matrix, y(i) corresponds to label of point x(i, :)
+y = [ones(1, class1) (-ones(1, class2))];
+
