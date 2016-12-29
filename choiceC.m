833e1898af2bba29f76b81b1fb1765b91d1c567d
Clémence Réda <clmence.reda@ens-cachan.fr>
2016-12-29 21:00:20 +0100

    presentation

--- choiceC.m
+++ choiceC.m
@@ -6,12 +6,13 @@ function C = choiceC(x, y)
 load crossvalidation.m;
 
 % Bounds for c
-c_max=5;
+c_max=3;
 c_min=1;
 
 C = c_min;
 mini = crossvalidation(C, x, y);
 for c_test=(c_min+1):c_max
+    c_test
     err = crossvalidation(c_test, x, y);
     if (mini > err)
         C = c_test;
